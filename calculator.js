f0d5652f4dd739cd567b149f1595956020d28fea
Macq Quentin <macqquentin@hotmail.fr>
2022-08-03 16:01:58 +0200

    js

--- calculator.js
+++ calculator.js
@@ -1,10 +1,56 @@
+const buttons = [
+    "(",
+    ")",
+    "C",
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "=",
+    "+",
+];
+
 const key = [...document.querySelectorAll('.buttons')];
 const keylist = key.map(key => key.dataset.key);
 const screen = document.querySelector('.screen');
 
 document.addEventListener('keydown', (e) => {
-    const value = e.keylist.toString();
-    console.log(value,typeof value)
-    //calc(value);
+ const value = e.key;
+console.log(value);
+    
+ });
 
-})
+document.addEventListener('click', (e) => {
+    const value = e.target.dataset.key;
+    console.log(value);
+
+});
+
+const calc= (value) => {
+    if (keylist.includes(value)) {
+      switch (value) {
+        case "delete":
+          Result.value = "";
+          break;
+        
+
+          
+           
+        }
+    }
+}
+
+          window.addEventListener("error", (e) => {
+            alert("Something wrong : " + e.message);
+          })
+        
